5afc118d113e394f4900b9e663702c05a16b6565
Ankur Singh <ankursingh9806@gmail.com>
2024-05-16 00:12:45 +0530

    User - isPremiumUser

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -21,6 +21,10 @@ const User = sequelize.define("User", {
         type: Sequelize.STRING,
         allowNull: false
     },
+    isPremiumUser: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    },
 });
 
 module.exports = User;
