7d59bdd05840efc48f7a7bcbb3cf56c4829184cc
Ankur Singh <ankursingh9806@gmail.com>
2024-05-15 20:15:29 +0530

    function - login

---
+++ frontend/js/login.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("login-form");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", login);
+
+async function login(e) {
+    try {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const loginDetails = {
+            email: email,
+            password: password
+        };
+        if (!loginDetails.email || !loginDetails.password) {
+            error.textContent = "Please fill out all fields.";
+            return;
+        }
+        const res = await axios.post("http://localhost:3000/user/login", loginDetails);
+        if (res.status === 200) {
+            window.location.href = "../html/expense.html";
+        } else {
+            error.textContent = "Login failed. Please try again later.";
+        }
+    } catch (err) {
+        if (err.response && err.response.status === 404) {
+            error.textContent = "Email not found. Please Signup.";
+        } else if (err.response && err.response.status === 401) {
+            error.textContent = "Incorrect password. Please try again.";
+        } else {
+            error.textContent = "An error occurred during Login. Please try again later.";
+        }
+    }
+}
