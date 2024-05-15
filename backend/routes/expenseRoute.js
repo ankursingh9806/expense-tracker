c9932ddc5f2732d88ef50a1246341a7991d5ca9e
Ankur Singh <ankursingh9806@gmail.com>
2024-05-15 22:24:35 +0530

    authenticate route - /expense-page, /expense-fetch, /expense-add, /expense-delete/:expenseId, /expense-update/:expenseId

--- backend/routes/expenseRoute.js
+++ backend/routes/expenseRoute.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const expenseController = require("../controllers/expenseController");
+const authentication = require("../middleware/authentication");
 
-router.get("/expense-page", expenseController.expensePage);
-router.get("/expense-fetch", expenseController.fetchExpense);
-router.post("/expense-add", expenseController.addExpense);
-router.delete("/expense-delete/:expenseId", expenseController.deleteExpense)
-router.put("/expense-update/:expenseId", expenseController.updateExpense)
+router.get("/expense-page", authentication.authenticate, expenseController.expensePage);
+router.get("/expense-fetch", authentication.authenticate, expenseController.fetchExpense);
+router.post("/expense-add", authentication.authenticate, expenseController.addExpense);
+router.delete("/expense-delete/:expenseId", authentication.authenticate, expenseController.deleteExpense)
+router.put("/expense-update/:expenseId", authentication.authenticate, expenseController.updateExpense)
 
 module.exports = router;
