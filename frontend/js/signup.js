b2f7a3d76b2c1681de80b2b21b887d79a6ebd1fd
Ankur Singh <ankursingh9806@gmail.com>
2024-05-15 20:15:50 +0530

    function - signup

---
+++ frontend/js/signup.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("signup-form");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", signup);
+
+async function signup(e) {
+    try {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const signupDetails = {
+            name: name,
+            email: email,
+            password: password
+        };
+        if (!signupDetails.name || !signupDetails.email || !signupDetails.password) {
+            error.textContent = "Please fill out all fields.";
+            return;
+        }
+        const res = await axios.post("http://localhost:3000/user/signup", signupDetails);
+        if (res.status === 201) {
+            window.location.href = "../html/login.html";
+        } else {
+            error.textContent = "Signup failed. Please try again later.";
+        }
+    } catch (err) {
+        if (err.response && err.response.status === 409) {
+            error.textContent = "Email already exists. Please Login.";
+        } else {
+            error.textContent = "An error occurred during Signup. Please try again later.";
+        }
+    }
+}
