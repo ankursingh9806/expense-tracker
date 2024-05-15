e386c4b0b1ee806be15ee04bace49f72fc7f0e12
Ankur Singh <ankursingh9806@gmail.com>
2024-05-16 00:11:36 +0530

    authenticate route - /is-premium-user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/userController");
+const authentication = require("../middleware/authentication");
 
 router.get("/signup-page", userController.signupPage);
 router.get("/login-page", userController.loginPage);
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
+router.get("/is-premium-user", authentication.authenticate, userController.isPremiumUser);
 
 module.exports = router;
