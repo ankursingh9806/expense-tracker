d48d83821d2ec20ac32ffba5f64bd67f279d33dd
Ankur Singh <ankursingh9806@gmail.com>
2024-05-18 11:43:00 +0530

    function - forgotPasswordPage, resetPasswordPage, forgotPassword, resetPassword

---
+++ backend/controllers/resetPasswordController.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
+var nodemailer = require("nodemailer");
+
+const User = require("../models/userModel");
+const ResetPassword = require("../models/resetPasswordModel");
+
+const forgotPasswordPage = async (req, res, next) => {
+    try {
+        res.sendFile(path.join(__dirname, "..", "..", "frontend", "html", "forgotPassword.html"));
+    } catch (err) {
+        console.error("error getting forgot password page", err);
+        res.status(500).json({ success: false, error: "internal server error" });
+    }
+};
+
+const resetPasswordPage = async (req, res, next) => {
+    try {
+        res.sendFile(path.join(__dirname, "..", "..", "frontend", "html", "resetPassword.html"));
+    } catch (err) {
+        console.error("error getting reset password page", err);
+        res.status(500).json({ success: false, error: "internal server error" });
+    }
+};
+
+const forgotPassword = async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ where: { email: email } });
+        if (!user) {
+            return res.status(404).json({ status: false, message: "user not found" });
+        }
+        const requestId = uuid.v4();
+        await ResetPassword.create({
+            UserId: user.id,
+            id: requestId,
+            active: true
+        });
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS
+            },
+        });
+        const mailOptions = {
+            from: process.env.USER,
+            to: email,
+            subject: "Reset your password",
+            html: `
+            <h1 style='color: seagreen;'>EXPENSE TRACKER</h1><br>
+            <h2>Reset password</h2><br>
+            <p>Please click on the link below to reset your account password:</p>
+            <a href="http://localhost:3000/password/reset-password-page/${requestId}" style='color: seagreen; font-weight: bold;'>Reset password</a>`
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("email sent: " + info.response);
+            }
+        });
+        console.log("email sent");
+        res.status(200).json({ status: true, message: "email sent" });
+    }
+    catch (err) {
+        console.error("error sending mail", err);
+        res.status(500).json({ status: false, error: "internal server error" });
+    }
+}
+
+const resetPassword = async (req, res) => {
+    try {
+        const { password } = req.body;
+        const resetId = req.params.resetId;
+
+        const resetRequest = await ResetPassword.findOne({ where: { id: resetId, active: true } });
+        if (!resetRequest) {
+            return res.status(400).json({ status: false, message: "expired password reset request" });
+        }
+
+        const user = await User.findOne({ where: { id: resetRequest.UserId } });
+        if (!user) {
+            return res.status(404).json({ status: false, message: "user not found" });
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        await User.update({ password: hashedPassword }, { where: { id: user.id } });
+        await ResetPassword.update({ active: false }, { where: { id: resetId } });
+
+        res.status(200).json({ status: true, message: "password updated" });
+    } catch (err) {
+        console.error("error updating password", err);
+        res.status(500).json({ status: false, error: "internal server error" });
+    }
+};
+
+module.exports = {
+    forgotPasswordPage,
+    forgotPassword,
+    resetPasswordPage,
+    resetPassword,
+}
