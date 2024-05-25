e036044d6ab482a06f554d87b3922ab890280028
Ankur Singh <ankursingh9806@gmail.com>
2024-05-25 23:35:21 +0530

    route - /report-page, /daily-report-view, /daily-report-download, /monthly-report-view, /monthly-report-download

---
+++ backend/routes/reportRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const reportController = require("../controllers/reportController");
+const authentication = require("../middleware/authentication");
+
+router.get("/report-page", reportController.reportPage);
+router.post("/daily-report-view", authentication.authenticate, reportController.dailyReportView);
+router.post("/daily-report-download", authentication.authenticate, reportController.dailyReportDownload);
+router.post("/monthly-report-view", authentication.authenticate, reportController.monthlyReportView);
+router.post("/monthly-report-download", authentication.authenticate, reportController.monthlyReportDownload);
+
+module.exports = router;
