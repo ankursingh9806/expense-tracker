cc0579558696b9037abb5a40d0dde1f2980f5f44
Ankur Singh <ankursingh9806@gmail.com>
2024-06-04 22:39:18 +0530

    allow any request other than predefined to login page

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use("/password", resetPasswordRoute);
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, "..", "frontend", "html", "login.html"));
 })
-
+ 
 User.hasMany(Expense);
 Expense.belongsTo(User);
 
