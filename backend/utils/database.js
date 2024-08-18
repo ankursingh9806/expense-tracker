3281b261818dc56d1700cdc22983367703084657
Ankur Singh <ankursingh9806@gmail.com>
2024-08-18 23:29:01 +0530

    mongoose

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,8 +1,22 @@
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect: "mysql",
-    host: process.env.DB_HOST
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     dialect: "mysql",
+//     host: process.env.DB_HOST
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log("Node.js application is connected to MongoDB")
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB", err);
+    });
+
+module.exports = mongoose;
